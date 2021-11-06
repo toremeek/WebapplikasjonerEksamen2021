eddca31a570e8ec57612978bf44b1e50b969cd03
Aleksander Lengard <aleksander.lengard@gmail.com>
2021-11-06 23:55:51 +0100

    Lagt til console.log for å se db feil meldinger

--- helpdesk/lib/api/dbErrors.js
+++ helpdesk/lib/api/dbErrors.js
@@ -2,18 +2,22 @@ export const DbError = {
   // TODO: Håndtere error logging
 
   create(resource, message, error) {
+    console.log(error)
     return { resource, message: message ?? `Failed creating ${resource}` }
   },
 
   read(resource, message, error) {
+    console.log(error)
     return { resource, message: message ?? `Failed finding ${resource}` }
   },
 
   update(resource, message, error) {
+    console.log(error)
     return { resource, message: message ?? `Failed updating ${resource}` }
   },
 
   delete(resource, message, error) {
+    console.log(error)
     return { resource, message: message ?? `Failed deleting ${resource}` }
   },
 }
