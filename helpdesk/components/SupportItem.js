e05504e4447ed395d068b4e9d36415f5e5a08788
toremeek <toremeek82@gmail.com>
2021-11-06 09:48:05 +0100

    oppdatert designet etter spec for alle henvendelser

--- helpdesk/components/SupportItem.js
+++ helpdesk/components/SupportItem.js
@@ -1,14 +1,72 @@
+import styled from 'styled-components'
+
 /* eslint-disable no-ternary */
 const SupportItem = ({ item }) => {
   const severityHigh = item?.severity === 'high' ? 'Høy' : null
   const severityMedium = item?.severity === 'medium' ? 'Medium' : null
   const severityLow = item?.severity === 'low' ? 'Lav' : null
 
+  // styled components for de ulike viktighetsgradene //
+  const HighSpan = styled.span`
+    display: flex;
+    font-weight: bold;
+    color: #b52424;
+    div {
+      background-color: #b52424;
+      width: 15px;
+      height: 15px;
+      border-radius: 40px;
+      margin: 5px;
+    }
+  `
+  const MediumSpan = styled.span`
+    display: flex;
+    font-weight: bold;
+    color: #dede2f;
+    div {
+      background-color: #dede2f;
+      width: 15px;
+      height: 15px;
+      border-radius: 40px;
+      margin: 5px;
+    }
+  `
+  const LowSpan = styled.span`
+    display: flex;
+    font-weight: bold;
+    color: #4a8c52;
+    div {
+      background-color: #4a8c52;
+      width: 15px;
+      height: 15px;
+      border-radius: 40px;
+      margin: 5px;
+    }
+  `
   return (
     <li className="issue">
       <div className="meta">
-        <span>{item?.department}</span>
-        <span>{severityHigh ?? severityMedium ?? severityLow}</span>
+        <span>
+          {item?.department.charAt(0).toUpperCase() + item?.department.slice(1)}
+        </span>
+        {severityHigh ? (
+          <HighSpan>
+            {severityHigh}
+            <div></div>
+          </HighSpan>
+        ) : null}
+        {severityMedium ? (
+          <MediumSpan>
+            {severityMedium}
+            <div></div>
+          </MediumSpan>
+        ) : null}
+        {severityLow ? (
+          <LowSpan>
+            {severityLow}
+            <div></div>
+          </LowSpan>
+        ) : null}
       </div>
       <h3>
         {item?.title} {item?.isResolved ? '(løst)' : null}
