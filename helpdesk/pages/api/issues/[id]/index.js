29e9d97dd4230da4d3c491063412f2034902ca94
Aleksander Lengard <aleksander.lengard@gmail.com>
2021-11-08 20:39:20 +0100

    Fjernet delte endepunkt - er ikke i krav spesifikasjonen

--- helpdesk/pages/api/issues/[id]/index.js
+++ helpdesk/pages/api/issues/[id]/index.js
@@ -1,24 +1,17 @@
 import * as issuesController from '@/features/issues/issues.controller'
+import { Response } from '@/lib/api/apiResponse'
 const handler = async (req, res) => {
-  const {
-    body,
-    method,
-    query: { id },
-  } = req
+  const { method } = req
 
   switch (method) {
     case 'GET':
       await issuesController.getIssueExtended(req, res)
       break
-    case 'DELETE':
-      // Legger til issue
-      res.status(200).json({ success: true, data: 'Fjernet 🐮' })
-      break
     case 'PUT':
       await issuesController.markIssueResovled(req, res)
       break
     default:
-      res.status(400).json({ success: false, error: 'Bad request 💀' })
+      return Response(res).badRequest()
   }
 }
 
