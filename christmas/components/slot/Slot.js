59f1fe65fe50e0a95ddfb826e52c2593c8083555
Aleksander Lengard <aleksander.lengard@gmail.com>
2021-11-15 23:40:46 +0100

    Dratt kode ut i komponenter

--- christmas/components/slot/Slot.js
+++ christmas/components/slot/Slot.js
@@ -1,19 +1,39 @@
 /* eslint-disable no-ternary */
+import { useEffect } from 'react'
+
+import { useCalenderDispatch } from '../../context/CalenderContext'
+import useApi from '@/hooks/useApi'
 import { daysUntil, isTimePassed } from '@/lib/dateHandler'
 
 const Slot = (props) => {
-  const { data, handler } = props
-  const { id, isOpen, openAt, order } = data
+  const dispatch = useCalenderDispatch()
+  const { slot } = props
+  const { id, isOpen, openAt, order } = slot
 
-  const handelOpenSlot = () => handler(id)
+  const { data, error, openSlot } = useApi()
+
+  const handelOpenSlot = () => openSlot(id)
+
+  useEffect(() => {
+    if (data?.success) dispatch({ type: 'OPEN_SLOT', id })
+  }, [data])
+
+  const buttonStyle = () => {
+    if (!isTimePassed(openAt)) return 'not-available'
+
+    return isOpen ? 'open' : 'can-open'
+  }
+
+  // TODO: Funksjon som definerer fargen på slot:
+  // Åpnet: grønn, Ikke åpnet, men tilgjengelig: Hvit, Ikke tilgjengelig: dusgrå
 
   return (
-    <div id="show" className={isOpen ? 'green' : 'normal'}>
-      {order}
+    <div className={`slot ${buttonStyle()}`} onClick={handelOpenSlot}>
+      <h1>{order}</h1>
       {isTimePassed(openAt) ? (
         <button onClick={handelOpenSlot}>Åpne nå</button>
       ) : (
-        <p>{`Kan åpnes om: ${daysUntil(openAt)} dager`}</p>
+        <p className="small">{`Åpner om ${daysUntil(openAt)} dager`}</p>
       )}
     </div>
   )
