0d8bf397e22830753904033d8cc73077b9e42921
Markus Lippestad <markusrl@live.no>
2021-11-15 19:07:19 +0100

    oppdatert visning med dato og egen knapp for å vise slug

--- christmas/pages/index.js
+++ christmas/pages/index.js
@@ -1,17 +1,27 @@
 import { useCalendar } from '@/hooks/useCalendar'
-import { useUser } from '@/hooks/useUser'
-import React, { useEffect, useRef, useState } from 'react'
-
+import { isStyledComponent } from 'styled-components'
+import {
+  msToDays,
+  formatDate,
+  daysUntil,
+  isTimePassed,
+} from '@/lib/dateHandler'
 export default function Home() {
   const { calendar } = useCalendar()
-  const [open, setOpen] = useState(false)
-  console.log(calendar)
-  let colors = []
-
   const dateFormater = (datestring) => new Date(datestring).toLocaleDateString()
+
   //todo: få denne slik at den luken man trykker på blir en annen farge, ikke alle sammen
+  //todo: får den fortsatt ikke til å fungere 🤦‍♂️
   const handleClick = (itemId) => {
-    console.log(itemId)
+    const updateSlot = calendar.slot.map(
+      (hatch) => (
+        //sjekker om itemId er den samme som hatch.id
+        hatch.id === itemId ? { ...hatch, isOpen: true } : hatch,
+        //prøver å vise slug'en for gitt luke, men ikke helt i mål
+        (document.getElementById('show').innerHTML = hatch.slug)
+      )
+    )
+    console.log(updateSlot)
   }
 
   return (
@@ -21,22 +31,23 @@ export default function Home() {
         <section id="calendar">
           {calendar?.slot?.map((item, index) => (
             <>
-              <div key={item.id}>
-                <button
-                  className={colors}
-                  key={index}
-                  type="button"
-                  onClick={() => handleClick(item.id)}
-                >
-                  {item.order} <br />
-                  {dateFormater(item.openAt)}
-                </button>
+              <div
+                id="show"
+                key={item.id}
+                className={item.isOpen ? 'green' : 'normal'}
+                key={index}
+              >
+                {item.order}
+                {isTimePassed(item.openAt) ? (
+                  <button onClick={() => handleClick(item.id)}>Åpne nå</button>
+                ) : (
+                  <p>{`Kan åpnes om: ${daysUntil(item.openAt)} dager`}</p>
+                )}
               </div>
             </>
           ))}
         </section>
       </div>
-      <div></div>
     </>
   )
 }
