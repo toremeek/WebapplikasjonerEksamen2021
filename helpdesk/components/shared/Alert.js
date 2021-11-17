00744832da4cb4432efd48111132dd8854a3963b
Aleksander Lengard <aleksander.lengard@gmail.com>
2021-11-17 22:58:12 +0100

    Laget lukke-knapp for alert melding

--- helpdesk/components/shared/Alert.js
+++ helpdesk/components/shared/Alert.js
@@ -1,7 +1,19 @@
+import { useState } from 'react'
+
 const Alert = (props) => {
+  const [visible, setVisible] = useState(true)
   const { role, text } = props
 
-  return <div className={`alert ${role}`}>{text}</div>
+  // TODO: Fikse styling på lukking av alert! Knapp helt til høyre osv osv.. :)
+  if (!visible) return null
+  return (
+    <div className={`alert ${role}`}>
+      {text}{' '}
+      <button type="button" onClick={() => setVisible(false)}>
+        Close
+      </button>
+    </div>
+  )
 }
 
 export default Alert
