8eadebfe2415d4e9755a511b86c7fa1fea5adf74
Aleksander Lengard <aleksander.lengard@gmail.com>
2021-11-17 22:21:50 +0100

    Forenklet endel av koden/logiken og flyttet ut i komponenter

--- helpdesk/pages/new-issue.js
+++ helpdesk/pages/new-issue.js
@@ -1,114 +1,76 @@
-import { validate } from '@/lib/Validation'
-import axios from 'axios'
-import { useRouter } from 'next/router'
-import { useState } from 'react'
+import PostIssueSuccess from '@/components/PostIssueSuccess'
+import Alert from '@/components/shared/Alert'
+import Loading from '@/components/shared/Loading'
+import ValidationAlert from '@/components/shared/ValidationAlert'
+import useApi from '@/hooks/useApi'
+import Validate from '@/lib/validation/validate'
+import { useEffect, useState } from 'react'
 
 const SupportForm = () => {
-  const router = useRouter()
-  const [validationErrors, setValidationErrors] = useState()
-  const [error, setError] = useState(false)
+  const [validationErrors, setValidationErrors] = useState([])
+  const { data, post, error, isLoading } = useApi()
   const [success, setSuccess] = useState(false)
+
   const [form, setForm] = useState({
     title: '',
     description: '',
     creator: '',
-    severity: '',
+    severity: null,
     department: '',
   })
 
-  const handleInputOnChange = ({ currentTarget: { name, value } }) =>
+  const handleInputChange = ({ currentTarget: { name, value } }) =>
     setForm((state) => ({ ...state, [name]: value }))
 
-  //Validere input før de sendes til api//
+  //Validere input før de sendes til api
   const handleSendSupport = async (event) => {
-    const sendData = {
-      title: form.title,
-      description: form.description,
-      creator: form.creator,
-      severity: parseInt(form.severity),
-      department: form.department,
-    }
     event.preventDefault()
-    const isValidTitle = validate.minMaxLength(25, 150, sendData.title)
-    const isValidDescription = validate.maxLength(250, sendData.description)
-    const isValidName = validate.nameCheck(sendData.creator)
+    const { isValid, issues } = Validate.issue(form)
 
-    if (!isValidDescription) {
-      setValidationErrors({
-        description: 'Beskrivelse må fylles ut, min 5 tegn og maks 250 tegn',
-      })
-    } else if (!isValidTitle) {
-      setValidationErrors({
-        title: 'Tittel må fylles ut, min 25 og maks 150 tegn',
-      })
-    } else if (!isValidName) {
-      setValidationErrors({
-        creator: 'For og etternavn må ha stor forbokstav og mellomrom',
-      })
-    } else {
+    if (isValid) {
       setValidationErrors(null)
-      await postForm(sendData)
-    }
-  }
-  const postForm = async (sendData) => {
-    try {
-      const response = await axios.post(
-        'http://localhost:3000/api/issues/',
-        sendData
-      )
-      if (response?.data.success) {
-        setSuccess(true)
-      }
-    } catch (err) {
-      console.log('noe gikk galt', err)
+      await postForm({ ...form, severity: parseInt(form.severity) })
+    } else {
+      console.log('ERROR:', issues)
+      setValidationErrors(issues)
     }
   }
 
-  const handleSuccess = () => {
-    setSuccess(false)
-    setForm({
-      title: '',
-      description: '',
-      creator: '',
-      severity: '',
-      department: '',
-    })
+  const postForm = async (issue) => {
+    await post(issue)
   }
 
-  const toAllIssues = () => {
-    router.push('/all-issues')
-  }
+  useEffect(() => {
+    if (data && !error) setSuccess(true)
+  }, [data])
+
+  if (success) return <PostIssueSuccess />
 
   return (
     <>
-      {success ? (
-        <div>
-          <p>Takk for din henvendelse</p>
-          <button type="button" onClick={handleSuccess}>
-            Ny sak
-          </button>
-          <button type="button" onClick={toAllIssues}>
-            Se alle saker
-          </button>
-        </div>
+      {isLoading ? (
+        <Loading />
       ) : (
         <>
           <h1>Ny henvendelse</h1>
+          {error ? (
+            <Alert
+              role="warning"
+              text={`Ooops, her skjedde det en feil: ${error}`}
+            />
+          ) : null}
           <form
             className="wrapper border light add-issue"
             onSubmit={handleSendSupport}
           >
             <section className="col-2">
-              {validationErrors?.department?.length > 0 ? (
-                <StyledErrorP>{validationErrors.department}</StyledErrorP>
-              ) : null}
               <label htmlFor="department-select">Avdeling:</label>
               <select
                 required
                 value={form.department}
                 id="department-select"
                 name="department"
-                onChange={handleInputOnChange}
+                onChange={handleInputChange}
               >
                 <option value="">Velg</option>
                 <option value="IT">IT</option>
@@ -117,16 +79,12 @@ const SupportForm = () => {
               </select>
 
               <label htmlFor="severity-select">Viktighet:</label>
-              {validationErrors?.severity?.length > 0 ? (
-                <StyledErrorP>{validationErrors.severity}</StyledErrorP>
-              ) : null}
-
               <select
                 required
                 value={form.severity}
                 name="severity"
                 id="severity-select"
-                onChange={handleInputOnChange}
+                onChange={handleInputChange}
               >
                 <option value="">Velg</option>
                 <option value="3">Høy</option>
@@ -136,51 +94,44 @@ const SupportForm = () => {
             </section>
 
             <section className="col-2">
-              {validationErrors?.title?.length > 0 ? (
-                <StyledErrorP>{validationErrors.title}</StyledErrorP>
-              ) : null}
               <label htmlFor="title">Tittel</label>
               <input
                 type="text"
                 id="title"
                 name="title"
                 placeholder="Tittel på henvendelsen"
-                onChange={handleInputOnChange}
+                onChange={handleInputChange}
                 value={form.title}
                 required
               />
-              {validationErrors?.creator?.length > 0 ? (
-                <StyledErrorP>{validationErrors.creator}</StyledErrorP>
-              ) : null}
               <label htmlFor="creator">Navn</label>
               <input
                 type="text"
                 id="creator"
                 placeholder="Fornavn Etternavn"
                 name="creator"
-                onChange={handleInputOnChange}
+                onChange={handleInputChange}
                 value={form.creator}
                 required
               />
             </section>
             <section className="col">
-              {validationErrors?.description?.length > 0 ? (
-                <StyledErrorP>{validationErrors.description}</StyledErrorP>
-              ) : null}
               <label htmlFor="description">Beskrivelse</label>
               <textarea
                 type="text"
                 id="description"
                 placeholder="Hva er problemet?"
                 name="description"
-                onChange={handleInputOnChange}
+                onChange={handleInputChange}
                 value={form.description}
                 rows="4"
                 required
               />
+              {validationErrors?.length > 0 ? (
+                <ValidationAlert errors={validationErrors} />
+              ) : null}
               <button type="sumbit">Send henvendelse</button>
             </section>
-            {error ? <p>Noe gikk galt.. {error}</p> : null}
           </form>
         </>
       )}
