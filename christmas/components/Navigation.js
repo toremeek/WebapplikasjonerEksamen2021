96bbbf6a9b2ade32c9d6b906c17b226d8a2d69ae
Aleksander Lengard <aleksander.lengard@gmail.com>
2021-11-22 11:24:30 +0100

    Lagt til title

--- christmas/components/Navigation.js
+++ christmas/components/Navigation.js
@@ -1,24 +1,27 @@
-import { useUser } from '@/hooks/useUser'
+/* eslint-disable no-ternary */
 import Link from 'next/link'
+
+import { useUser } from '@/hooks/useUser'
 
 const Navigation = () => {
   const { admin } = useUser()
+
   return (
-    <nav>
-      <ul>
-        <li>
-          <Link href="/">Hjem</Link>
-        </li>
-        {
-          //Sjekker om bruker er admin, hvis ja, kan navigere til dashboardsiden
-          admin ? (
+    <header>
+      <h1>Julekalender</h1>
+      <nav>
+        <ul>
+          <li>
+            <Link href="/">Hjem</Link>
+          </li>
+          {admin ? (
             <li>
               <Link href="/dashboard">Admin</Link>
             </li>
-          ) : null
-        }
-      </ul>
-    </nav>
+          ) : null}
+        </ul>
+      </nav>
+    </header>
   )
 }
 
