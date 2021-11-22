b5e4d0ace04d377f48eebc7d91f0be77c28b5cfd
Aleksander Lengard <aleksander.lengard@gmail.com>
2021-11-23 00:15:12 +0100

    Hook til å skru av / på - eks modal

---
+++ christmas/hooks/useToggle.js
@@ -0,0 +1,10 @@
+import { useCallback, useState } from 'react'
+
+const useToggle = (initialState) => {
+  const [isToggled, setIsToggled] = useState(initialState)
+  const toggle = useCallback(() => setIsToggled(!isToggled), [isToggled])
+
+  return [isToggled, toggle]
+}
+
+export default useToggle
