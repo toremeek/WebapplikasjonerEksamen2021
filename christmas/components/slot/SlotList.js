035678ff21ac8770e5e599b409dcf91d7f6e9bc9
Aleksander Lengard <aleksander.lengard@gmail.com>
2021-11-22 02:14:35 +0100

    Lagt til context - useapi oppdaterer context

--- christmas/components/slot/SlotList.js
+++ christmas/components/slot/SlotList.js
@@ -1,9 +1,21 @@
-import { useCalender } from '../../context/CalenderContext'
+import Alert from '../shared/Alert'
 import Slot from './Slot'
+import { useCalenderContext } from '@/context/CalenderContext'
 
 const SlotList = () => {
-  const { state } = useCalender()
+  const { state } = useCalenderContext()
 
+  // Hvis kalender har blitt lastet men vi mangler luker!
+  if (state?.slot?.length <= 0) {
+    return (
+      <Alert
+        role="danger"
+        text={'⚠️ Her har det skjedd en feil! Finner ingen luker!'}
+      />
+    )
+  }
+
+  // Viser luker
   return (
     <section className="calendar">
       {state?.slot?.map((obj, index) => (
--- christmas/pages/index.js
+++ christmas/pages/index.js
@@ -1,32 +1,35 @@
 /* eslint-disable no-ternary */
 import { useEffect } from 'react'
 
-import { CalenderProvider } from '../context/CalenderContext'
 import Alert from '@/components/shared/Alert'
 import Loading from '@/components/shared/Loading'
 import SlotList from '@/components/slot/SlotList'
 import useApi from '@/hooks/useApi'
+import { useUser } from '@/hooks/useUser'
 
 export default function Home() {
-  const { isLoading, data, get, error } = useApi()
+  const { isLoading, getCalender, error } = useApi()
+  const {
+    user: { id, username },
+  } = useUser()
 
+  // Laster kalender fra databasen
   useEffect(() => {
-    get('calenders?name=Julekalender')
-  }, [get])
+    getCalender('Julekalender')
+  }, [])
 
   if (error)
-    return <Alert role="warnign" text={`⚠️ En feil har oppstått: ${error}`} />
+    return <Alert role="danger" text={`⚠️ En feil har oppstått: ${error}`} />
+
+  console.log(id, username)
 
   return (
     <>
       <h1>Julekalender eksamen 2021</h1>
-      {isLoading ? (
-        <Loading />
-      ) : (
-        <CalenderProvider value={data}>
-          <SlotList />
-        </CalenderProvider>
-      )}
+      {!id || !username ? (
+        <Alert role="info" text="Vennligst logg inn for å åpne luker!" />
+      ) : null}
+      {isLoading ? <Loading /> : <SlotList />}
     </>
   )
 }
