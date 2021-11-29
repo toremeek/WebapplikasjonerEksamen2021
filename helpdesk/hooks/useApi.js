f368ef96ae19d468469c0bedb39a8b23f4b6305c
Aleksander Lengard <aleksander.lengard@gmail.com>
2021-11-29 23:28:22 +0100

    Flyttet oppdatering av context ut i komponenter

--- helpdesk/hooks/useApi.js
+++ helpdesk/hooks/useApi.js
@@ -3,15 +3,11 @@ import { useState } from 'react'
 
 import axios from 'axios'
 
-import { useIssueContext } from '@/context/IssuesContext'
-
 const useApi = () => {
   const [isLoading, setIsLoading] = useState(false)
   const [data, setData] = useState()
   const [error, setError] = useState()
-  const { dispatch } = useIssueContext()
 
-  // API FUNCTIONS
   // GET
   const get = async (url = '') => {
     setIsLoading(true)
@@ -56,7 +52,7 @@ const useApi = () => {
   const put = async (id) => {
     setIsLoading(true)
     try {
-      const response = await axios.put(`/api/issues/${id}`)
+      const response = await axios.put(`/api/issues/${id}asdasd`)
       const { data, success } = response.data
 
       if (success) setData(data)
@@ -65,54 +61,10 @@ const useApi = () => {
 
       return data
     } catch (err) {
-      setError(err)
+      setError(err.response?.data?.error)
       setIsLoading(false)
 
       return err
-    }
-  }
-
-  // API Funksjoner som oppdaterer context
-  // TODO: Burde flyttes til egen hook?
-  // Henter alle henvendelser, oppdaterer context
-  const getIssues = async () => {
-    const result = await get()
-
-    if (!error) dispatch({ type: 'SET_ISSUES', issues: result })
-  }
-
-  // Henter filtrerte henvendelser fra api
-  const getFilterIssues = async (filter) => {
-    const { property, value } = filter
-    const result = await get(`${property}/${value}`)
-
-    if (!error) dispatch({ type: 'SET_ISSUES', issues: result })
-  }
-
-  // Henter en henvendelse med id
-  const getIssue = async (id) => {
-    const result = await get(id)
-
-    if (!error) dispatch({ type: 'SET_ISSUE', issue: result })
-  }
-
-  // Merker en henvendelse som løst / avsluttet
-  const resolve = async (id) => {
-    const result = await put(id)
-
-    if (!error) dispatch({ type: 'SET_ISSUE', issue: result })
-  }
-
-  // Legger til kommentar til en henvendelse
-  const postComment = async (comment, id) => {
-    const result = await post({ comment }, `${id}/comments`)
-
-    if (!error) {
-      dispatch({
-        type: 'ADD_NEW_COMMENT_TO_ISSUE',
-        comment: result,
-        issueId: id,
-      })
     }
   }
 
@@ -122,11 +74,7 @@ const useApi = () => {
     get,
     error,
     post,
-    resolve,
-    getIssues,
-    getIssue,
-    getFilterIssues,
-    postComment,
+    put,
   }
 }
 
