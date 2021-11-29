f22fa97801a55ee09f853781c7fc4b8d781e26f0
Aleksander Lengard <aleksander.lengard@gmail.com>
2021-11-29 22:48:08 +0100

    Lag til noe teskt med lenker på hjem siden

--- helpdesk/pages/index.js
+++ helpdesk/pages/index.js
@@ -1,4 +1,20 @@
-// TODO: Lage en side som henter én issue (siste?)
-export default function Home() {
-  return <p>Trenger noe nytt her....</p>
-}
+import Link from 'next/link'
+
+const Home = () => (
+  <>
+    <h1>Helpdesk</h1>
+    <p className="wrapper border light">
+      I denne helpdesken kan du{' '}
+      <Link href="/new-issue">
+        <a>legge til</a>
+      </Link>{' '}
+      henvendelser du trenger hjelp til. Du kan også ta en titt på{' '}
+      <Link href="/all-issues">
+        <a>andre sine</a>
+      </Link>{' '}
+      problemer og prøve å hjelpe dem.
+    </p>
+  </>
+)
+
+export default Home
